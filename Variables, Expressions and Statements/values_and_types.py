f8f264d9244c8c621085bf1ba4d20cce6143d2cb
emnagc6 <emn.agc6@gmail.com>
2023-10-20 16:22:14 +0300

    added values_and_types.py

---
+++ Variables, Expressions and Statements/values_and_types.py
@@ -0,0 +1,24 @@
+#Strings
+print("hello world")
+print('hello world')
+print("""hello
+world""")
+
+#Integers
+print(11)
+print(1_000_000_000)
+print(43+55)
+print("13") #prints as string
+
+#Float
+print(3.14159)
+
+#Boolean
+True
+False
+
+#Type Fuction
+print(type(5)) #<class 'int'>
+print(type("hello!")) #<class 'str'>
+print(type("515")) #<class 'str'>
+print(type(True)) #<class 'bool'>
